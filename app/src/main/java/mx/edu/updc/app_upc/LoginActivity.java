1642bf77b9ebd5771757aa81f9c23e43e20b73ba
abrahamx97 <abrahamx97@gmail.com>
2016-12-01 15:02:24 -0600

    modificaciones para lectura del json

--- app/src/main/java/mx/edu/updc/app_upc/LoginActivity.java
+++ app/src/main/java/mx/edu/updc/app_upc/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,52 +28,37 @@ import cz.msebera.android.httpclient.Header;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText txtUsuario, txtContrasena;
-    TextView textEstado;
-
-    private Handler puente = new Handler(){
-        @Override
-        public void handleMessage(Message msg){
-            Toast.makeText(getApplicationContext(), msg.obj.toString(),
-            Toast.LENGTH_LONG).show();
-        }
-    };
+    public final int activo=1;
+    public final int baja=0;
+    EditText txtContrasena;
+    AutoCompleteTextView txtUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        txtUsuario = (EditText) findViewById(R.id.textUsuario);
+        txtUsuario = (AutoCompleteTextView) findViewById(R.id.textUsuario);
         txtContrasena = (EditText) findViewById(R.id.textContrasena);
-        textEstado = (TextView) findViewById(R.id.textEstado);
 
     }
 
     public void login(View v){
-        if (txtUsuario.getText().length()==0){
-            txtUsuario.requestFocus();
-            //SALIR DEL EVENTO
-        }
-
-        if (txtContrasena.getText().length()==0){
-            txtContrasena.requestFocus();
-            //salir del evento
-        }
-
         String usuario = txtUsuario.getText().toString() ;
         String contrasena = txtContrasena.getText().toString();
 
-        enviarDatos(usuario,contrasena);
-
-
-        txtUsuario.setText("");
-        txtContrasena.setText("");
+        if (usuario.trim().length()==0 ){
+            txtUsuario.requestFocus();
+        }else if (contrasena.trim().length()==0){
+            txtContrasena.requestFocus();
+        }else {
+            conectar(usuario, contrasena);
+        }
 
     }
 
 
-    private void enviarDatos(String usuario, String contrasena){
+    private void conectar(String usuario, String contrasena){
         AsyncHttpClient client = new AsyncHttpClient();
         String url="http://192.168.43.204/appupc/login.php";
         RequestParams rq=new RequestParams();
@@ -83,20 +69,29 @@ public class LoginActivity extends AppCompatActivity {
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 super.onSuccess(statusCode, headers, response);
                 try {
-                    String estado = "0";
-                    if (response.getString("estado").equals("1")) {
-                        estado = "1";
-                    };
-                    textEstado.setText(estado);
+                    int encontrado=1;
+                    if (response.getInt("estado")==encontrado){
+                        if (response.getInt("activo")==activo) {
+                            int id_profesor = response.getInt("id_profesor");
+
+                        }else if (response.getInt("activo")==baja){
+                            //SI EL USUARIO ESTA DADO DE BAJA
+                        }
+                    }else{
+                        //NO SE ENCONTRO USUARIO
+                    }
+
+
                 } catch (JSONException e) {
                     e.printStackTrace();
+
                 }
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
                 super.onFailure(statusCode, headers, responseString, throwable);
-                textEstado.setText("No paso");
+                //NO SE RETORNO UN JSON VALIDO
             }
         });
 
